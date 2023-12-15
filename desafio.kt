fa0437f756b75ab587cdf5daceab96d33180164e
Rafael Oliveira <75651763+rafa3637@users.noreply.github.com>
2023-12-14 23:09:58 -0300

    primeiro commit

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,47 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+// desafio.kt
 
-class Usuario
+// Representa um conteúdo educacional
+data class ConteudoEducacional(val titulo: String, val descricao: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+// Representa um aluno
+data class Aluno(val nome: String, val matricula: String)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+// Interface para definir comportamentos comuns a Formacoes
+interface Formacao {
+    val nome: String
+    val nivel: String
+    val conteudosEducacionais: List<ConteudoEducacional>
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricularAluno(aluno: Aluno)
+}
+
+// Implementação básica da interface Formacao
+class FormacaoImpl(
+    override val nome: String,
+    override val nivel: String,
+    override val conteudosEducacionais: List<ConteudoEducacional>
+) : Formacao {
+    private val alunosMatriculados = mutableListOf<Aluno>()
+
+    override fun matricularAluno(aluno: Aluno) {
+        alunosMatriculados.add(aluno)
+        println("Aluno ${aluno.nome} matriculado na formação $nome")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Exemplo de uso
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", "Aprenda os conceitos básicos do Kotlin")
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", "Entenda POO com Kotlin")
+    val conteudos = listOf(conteudo1, conteudo2)
+
+    val formacaoKotlin = FormacaoImpl("Formação Kotlin", "Intermediário", conteudos)
+
+    val aluno1 = Aluno("João", "12345")
+    val aluno2 = Aluno("Maria", "67890")
+
+    formacaoKotlin.matricularAluno(aluno1)
+    formacaoKotlin.matricularAluno(aluno2)
 }
